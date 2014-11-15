6d3c70f168c817555f6eb7013013e18ef1ca1afe
Andreas Tennert <mail@andreas-tennert.de>
2014-11-15 18:16:34 +0100

    Added map data container to hand set of new values to application.

---
+++ com/src/main/java/org/atennert/com/communication/MapDataContainer.java
@@ -0,0 +1,12 @@
+
+package org.atennert.com.communication;
+
+import java.util.Map;
+
+public class MapDataContainer extends DataContainer
+{
+    public MapDataContainer(Map<String, Object> data)
+    {
+        super(null, data);
+    }
+}
